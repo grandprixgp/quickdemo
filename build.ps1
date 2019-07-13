7be736b7ed280ddfa4e40048b97209d2ef6567a0
grandprixgp <akala@porkpi.com>
2019-07-13 16:46:30 +0100

    build output was ambigious

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-$env:GOOS="windows";$env:GOARCH="amd64"; go build
-$env:GOOS="linux";$env:GOARCH="amd64"; go build
+$env:GOOS="windows";$env:GOARCH="amd64"; go build -o quickdemo.exe
+$env:GOOS="linux";$env:GOARCH="amd64"; go build -o quickdemo.bin
