5d66d4f27fc5e3e1611c1e0ea73a615bd4e3962f
grandprixgp <akala@porkpi.com>
2019-08-04 09:33:31 +0100

    latest build

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 # executables
-xgo --targets=linux/amd64,windows/amd64 -out build/release/quickdemo .
+xgo --targets=linux/amd64,windows/amd64 -out build/quickdemo .
 # modules
-xgo -buildmode=c-shared --targets=linux/amd64,windows/amd64 -out build/release/quickdemo .
+xgo -buildmode=c-shared --targets=linux/amd64,windows/amd64 -out build/quickdemo .
